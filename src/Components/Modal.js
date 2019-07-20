4dfd172693549c3697005b0de52b03f1154d24a3
annlinros <annlinros@gmail.com>
2019-07-20 17:58:59 +0530

    Modal cmponent

---
+++ src/Components/Modal.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import { ProductConsumer } from "../context";
+import { Link } from "react-router-dom";
+import { ButtonElement } from "./Button";
+
+export default class Modal extends Component {
+  render() {
+    return (
+      <ProductConsumer>
+        {value => {
+          const { img, title, price } = value.modalProduct;
+          return (
+            <ModalContainer>
+              <div className="container">
+                <div className="row">
+                  <div
+                    id="modal"
+                    className="col-8 mx-auto col-md-6 col-lg-4 text-center p-3"
+                  >
+                    <h5>Item added to Cart!</h5>
+                    <img
+                      className="img-fluid pb-3"
+                      src={img}
+                      alt={title}
+                    />
+                    <h5>{title}</h5>
+                    <h3>Price: ${price}</h3>
+                    <Link to="/">
+                      <ButtonElement>Continue shopping</ButtonElement>
+                    </Link>{" "}
+                    <Link to="/cart">
+                      <ButtonElement>View CART</ButtonElement>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </ModalContainer>
+          );
+        }}
+      </ProductConsumer>
+    );
+   }
+}
+const ModalContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  color:#fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  #modal {
+    background: #000;
+  }
+`;
