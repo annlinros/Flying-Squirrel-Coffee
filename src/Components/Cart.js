80c892eb23263131a32ceb552c03264b4993e346
annlinros <annlinros@gmail.com>
2019-07-21 12:56:21 +0530

    renders empty cart

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,19 +1,36 @@
 import React, { Component } from "react";
 import { ProductConsumer } from "../context";
-import CartColumns from './CartColumns'
+import CartColumns from "./CartColumns";
+import { Link } from "react-router-dom";
+
 
 export default class Cart extends Component {
   render() {
     return (
-      <React.Fragment>
-        <h1 className="title">Your Cart</h1>
-        <CartColumns />
-        <ProductConsumer>
-            {value => {
-                value.cart.map(item=> <CartColumns item={item}/> )
-            }}
-        </ProductConsumer>
-      </React.Fragment>
+      <ProductConsumer>
+        {value => {
+          const { cart } = value;
+          if (cart.length > 0) {
+            return (
+              <React.Fragment>
+                <h1 className="title">Your Cart</h1>
+                <CartColumns />
+              </React.Fragment>
+            );
+          } else {
+            return (
+              <div className="d-flex flex-column align-items-center p-5">
+                <h1 className="text-center">Your cart is empty!</h1>
+                <Link to="/">
+                  <button className="btn btn-outline-secondary">
+                    Start shopping!
+                  </button>
+                </Link>
+              </div>
+            );
+          }
+        }}
+      </ProductConsumer>
     );
   }
 }
