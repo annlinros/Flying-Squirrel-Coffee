ada979727635ee31ba98957713076b1b78ad29b4
annlinros <annlinros@gmail.com>
2019-07-22 17:09:41 +0530

    added functional methods to the cart

--- src/context.js
+++ src/context.js
@@ -7,11 +7,11 @@ class ProductProvider extends Component {
   state = {
     products: [],
     detailProduct,
-    cart: [],
+    cart: storeProducts,
     modalProduct: detailProduct,
     modalOpen: false
   };
-//  setting "products on state"
+  //  setting "products on state"
   componentDidMount() {
     this.setProducts();
   }
@@ -19,53 +19,73 @@ class ProductProvider extends Component {
   setProducts = () => {
     let products = [];
     storeProducts.forEach(item => {
-      const singleItem = {...item };
+      const singleItem = { ...item };
       products = [...products, singleItem];
     });
-    this.setState({ 
-      products 
+    this.setState({
+      products
     });
   };
-// Handling the click event to show Product details.
+  // Handling the click event to show Product details.
 
-  getProduct = (id) => {
-  return this.state.products.find(item=> item.id === id)
- }
-  handleDetail = (id) => {
-   const product = this.getProduct(id)
-   this.setState({
-     detailProduct: product
-   })
+  getProduct = id => {
+    return this.state.products.find(item => item.id === id);
+  };
+  handleDetail = id => {
+    const product = this.getProduct(id);
+    this.setState({
+      detailProduct: product
+    });
   };
 
   // Add to Cart
 
-  addToCart = (id) => {
+  addToCart = id => {
     const cartItem = this.getProduct(id);
     cartItem.count = 1;
     const price = cartItem.price;
-    cartItem.total= price;
-    this.setState({
-      cart: [...this.state.cart,cartItem]
-    },() => console.log(this.state.modalOpen))
+    cartItem.total = price;
+    this.setState(
+      {
+        cart: [...this.state.cart, cartItem]
+      },
+      () => console.log("aded to cart", this.state.cart)
+    );
   };
 
   // Opening and Closing of Modal upon adding items to the cart
 
-  openModal = (id) => {
+  openModal = id => {
     const product = this.getProduct(id);
     console.log(product);
-    this.setState(
-      {
-        modalProduct: product,
-        modalOpen: true
-      });
-  }
+    this.setState({
+      modalProduct: product,
+      modalOpen: true
+    });
+  };
   closeModal = () => {
-   this.setState({
-     modalOpen: false
-   },() => console.log(this.state.modalOpen)  )    
-  }
+    this.setState({
+      modalOpen: false
+    });
+  };
+  // Increment and decrement product count in cart
+
+  incrementCount = id => {
+    console.log("incremented");
+  };
+
+  decrementCount = id => {
+    console.log("decremented");
+  };
+  // Remove item from cart
+  clearCartItem = id => {
+    console.log("item cleared");
+  };
+
+  // Reset cart
+  resetCart = id => {
+    console.log("cart resetted");
+  };
 
   render() {
     return (
@@ -75,7 +95,11 @@ class ProductProvider extends Component {
           handleDetail: this.handleDetail,
           addToCart: this.addToCart,
           openModal: this.openModal,
-          closeModal: this.closeModal
+          closeModal: this.closeModal,
+          incrementCount:this.incrementCount,
+          decrementCount: this.decrementCount,
+          clearCartItem:this.clearCartItem,
+          resetCart:this.resetCart
         }}
       >
         {this.props.children}
